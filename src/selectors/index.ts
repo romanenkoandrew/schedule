14e68c662c8dc2da4573bcc7815b3078f456a869
Artem <artem.antonov1993@gmail.com>
2020-09-13 14:33:10 +0300

    refactor: correct file with selectors

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -4,20 +4,11 @@ import get from 'lodash/get';
 export const MainSelector = (state: any) => get(state, 'main');
 export const scoreSelector = createSelector(MainSelector, main => main.score);
 
-export const EventsDataSelector = (state: any) => get(state, 'data');
-export const eventsDataSelector = createSelector(EventsDataSelector, data => data.eventsData);
+export const DataSelector = (state: any) => get(state, 'data');
 
-export const EventDataSelector = (state: any) => get(state, 'data');
-export const eventDataSelector = createSelector(EventDataSelector, data => data.eventData);
-
-export const OrganizersDataSelector = (state: any) => get(state, 'data');
-export const organizersDataSelector = createSelector(EventsDataSelector, data => data.organizersData);
-
-export const OrganizerDataSelector = (state: any) => get(state, 'data');
-export const organizerDataSelector = createSelector(EventDataSelector, data => data.organizerData);
-
-export const LoadingDataSelector = (state: any) => get(state, 'data');
-export const loadingDataSelector = createSelector(LoadingDataSelector, data => data.loading);
-
-export const ErrorDataSelector = (state: any) => get(state, 'data');
-export const errorDataSelector = createSelector(ErrorDataSelector, data => data.error);
+export const eventsDataSelector = createSelector(DataSelector, data => data.eventsData);
+export const eventDataSelector = createSelector(DataSelector, data => data.eventData);
+export const organizersDataSelector = createSelector(DataSelector, data => data.organizersData);
+export const organizerDataSelector = createSelector(DataSelector, data => data.organizerData);
+export const loadingDataSelector = createSelector(DataSelector, data => data.loading);
+export const errorDataSelector = createSelector(DataSelector, data => data.error);
