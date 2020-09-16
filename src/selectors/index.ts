bad5bc895f1c6f08723a7b0e8e09c83f19c5f187
Natein <natein@gmail.com>
2020-09-17 02:23:55 +0300

    feat: header

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -3,6 +3,10 @@ import get from 'lodash/get';
 
 export const MainSelector = (state: any) => get(state, 'main');
 export const scoreSelector = createSelector(MainSelector, main => main.score);
+export const switchMentorStudentSelector = createSelector(MainSelector, main => main.isStudent);
+export const switchLayoutSelector = createSelector(MainSelector, main => main.layout);
+export const changeTimezoneSelector = createSelector(MainSelector, main => main.timezone);
+export const changeCourseSelector = createSelector(MainSelector, main => main.courses);
 
 export const DataSelector = (state: any) => get(state, 'data');
 
@@ -12,3 +16,6 @@ export const organizersDataSelector = createSelector(DataSelector, data => data.
 export const organizerDataSelector = createSelector(DataSelector, data => data.organizerData);
 export const loadingDataSelector = createSelector(DataSelector, data => data.loading);
 export const errorDataSelector = createSelector(DataSelector, data => data.error);
+
+
+
