7d21248cfc53ea898bf9d126f0231285db2db20b
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-09-18 16:05:09 +0300

    feat: add basic function and styles

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,230 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { Calendar, Tag } from 'antd';
+import { Calendar, Tag, Select, Radio, Col, Row, Typography, Button } from 'antd';
+import React, { useEffect, useState } from 'react';
 
-import './Calendar.css';
 import 'antd/dist/antd.css';
+import './Calendar.css';
 
-function getListData(value: { date: () => any }) {
-  let listData;
-  console.log(value.date());
-  console.log(value.date());
-  switch (value.date()) {
-    case 8:
-      listData = [
-        {
-          date: '',
-          time: '',
-          type: 'task',
-          content: 'Codewars stage#1',
-          link: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/codewars-stage-1.md',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'test',
-          content: 'Тест как учиться в RSSchool',
-          link: 'Тест по основам Git. Ссылка будет в Discord. Его необходимо пройти за 24 часа',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'education',
-          content: 'JavaScript Intro',
-          link: 'https://www.youtube.com/watch?v=cP08joG36F0',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        }
-      ];
-      break;
-    case 10:
-      listData = [
-        {
-          date: '',
-          time: '',
-          type: 'task',
-          content: 'Codewars stage#1',
-          link: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/codewars-stage-1.md',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'test',
-          content: 'Тест как учиться в RSSchool',
-          link: 'Тест по основам Git. Ссылка будет в Discord. Его необходимо пройти за 24 часа',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'education',
-          content: 'JavaScript Intro',
-          link: 'https://www.youtube.com/watch?v=cP08joG36F0',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        }
-      ];
-      break;
-    case 15:
-      listData = [
-        {
-          date: '',
-          time: '',
-          type: 'task',
-          content: 'Codewars stage#1',
-          link: 'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/codewars-stage-1.md',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'test',
-          content: 'Тест как учиться в RSSchool',
-          link: 'Тест по основам Git. Ссылка будет в Discord. Его необходимо пройти за 24 часа',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'education',
-          content: 'JavaScript Intro',
-          link: 'https://www.youtube.com/watch?v=cP08joG36F0',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        },
-        {
-          date: '',
-          time: '',
-          type: 'live',
-          content: 'Git Basics. Live coding',
-          link: 'Ответы на вопросы по алгоритмическим заданиям Stage#1',
-          timeLong: '',
-          teacher: ''
-        }
-      ];
-    default:
+const listData = [
+  {
+    id: 'KyvcYfdhjMuXQeK4eoYc',
+    name: 'HTML',
+    description: 'Студент знакомится с HTML',
+    descriptionUrl: 'https://guides.hexlet.io/markdown/',
+    type: ['htmlTask', 'info'],
+    timeZone: 3,
+    dateTime: 1600291763391,
+    place: 'class',
+    comment: 'Создан и размещён на gh - pages файл HTML',
+    trainee: 'Сергей Шаляпин',
+    courseName: 'jsFrontEnd',
+    timeToImplementation: 4,
+    broadcastUrl: 'Link on Video',
+    materialsLinks: ['https: // github.com/rolling-scopes-school/tasks/blob/master/tasks/codewars-stage-1.md', 'link2'],
+    block: 'HTML',
+    result: 'Студент знает HTML',
+    stack: ['HTML', 'CSS', 'Markdown'],
+    feedBack: 'Cool',
+    taskBreakpoints: [1600291763391, 1600291764391],
+    videoLink: 'string'
+  },
+  {
+    id: 'KyvcYfdhjMuXQeK4eoYc',
+    name: 'HTML',
+    description: 'Студент знакомится с HTML',
+    descriptionUrl: 'https://guides.hexlet.io/markdown/',
+    type: ['deadline', 'info'],
+    timeZone: 3,
+    dateTime: 1600391764391,
+    place: 'class',
+    comment: 'Создан и размещён на gh - pages файл HTML',
+    trainee: 'Сергей Шаляпин',
+    courseName: 'jsFrontEnd',
+    timeToImplementation: 4,
+    broadcastUrl: 'Link on Video',
+    materialsLinks: ['https: // github.com/rolling-scopes-school/tasks/blob/master/tasks/codewars-stage-1.md', 'link2'],
+    block: 'HTML',
+    result: 'Студент знает HTML',
+    stack: ['HTML', 'CSS', 'Markdown'],
+    feedBack: 'Cool',
+    taskBreakpoints: [1600291763391, 1600291764391],
+    videoLink: 'string'
   }
-  return listData || [];
+];
+
+function getListData(value: { date: () => number; month: () => number }) {
+  const data: any = [];
+  listData.map(item => {
+    let eventStart = new Date(item.dateTime);
+    if (eventStart.getUTCMonth() !== value.month()) {
+      return;
+    }
+    if (eventStart.getUTCDay() === value.date()) {
+      data.push(item);
+    }
+  });
+  return data;
 }
 
-function dateCellRender(value: { date: () => any }) {
+function dateCellRender(value: { date: () => number; month: () => number }) {
   const listData = getListData(value);
   return (
     <ul className="events">
-      {listData.map(item => (
-        <li key={item.type}>
-          <Tag className={item.type}>
-            <a href={item.link}>{item.content}</a>
-          </Tag>
-        </li>
-      ))}
+      {listData.map(
+        (item: { type: (string | undefined)[]; descriptionUrl: string | undefined; description: React.ReactNode }) => (
+          <li key={item.type[0]}>
+            <Tag className={item.type[0]}>
+              <a href={item.descriptionUrl}>{item.description}</a>
+            </Tag>
+          </li>
+        )
+      )}
     </ul>
   );
 }
@@ -243,6 +96,10 @@ function monthCellRender(value: any) {
       <span>Backlog number</span>
     </div>
   ) : null;
+}
+
+function onPanelChange(value: any, mode: any) {
+  console.log(value, mode);
 }
 
 const CalendarApp: React.FC<any> = () => {
